9ab2886096a9ef6a9cf6c0fe2a3820ada7b9ca4c
root <root@localhost.localdomain>
2026-06-10 13:54:36 +0800

    fixed: makebuild.sh

--- kernel/makebuild.sh
+++ kernel/makebuild.sh
@@ -54,8 +54,8 @@ main()
         elif [ "x"$1 == "xinstall" ]; then
                 kernel_install
                 exit 0
-        elif 
-		"Error:"
+        else 
+		"Error: (clean | make | install)"
 		exit 1
 	fi
 }
